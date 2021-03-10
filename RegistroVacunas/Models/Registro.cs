8ff13cac6d4b06a8a86366a033dd9e7bf3b4db3d
Eimy C. Olivo G <20197830@gmail.com>
2021-03-09 20:10:50 -0400

    inicial

---
+++ RegistroVacunas/Models/Registro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace RegistroVacunas.Models
+{
+    public partial class Registro
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+        public string Telefono { get; set; }
+        public TimeSpan FechaNacimiento { get; set; }
+        public string Provincia { get; set; }
+        public bool Vacuna { get; set; }
+        public TimeSpan _1eraDosis { get; set; }
+        public TimeSpan? _2daDosis { get; set; }
+    }
+}
---
+++ RegistroVacunas/Models/registro_vacunasContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace RegistroVacunas.Models
+{
+    public partial class registro_vacunasContext : DbContext
+    {
+        public registro_vacunasContext()
+        {
+        }
+
+        public registro_vacunasContext(DbContextOptions<registro_vacunasContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Registro> Registro { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=localhost;uid=root;pwd=mysql;database=registro_vacunas", x => x.ServerVersion("8.0.18-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Registro>(entity =>
+            {
+                entity.ToTable("registro");
+
+                entity.Property(e => e.Id).HasColumnType("int(10)");
+
+                entity.Property(e => e.Apellido)
+                    .IsRequired()
+                    .HasColumnType("tinytext")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                entity.Property(e => e.Cedula)
+                    .IsRequired()
+                    .HasColumnType("tinytext")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                entity.Property(e => e.FechaNacimiento)
+                    .HasColumnName("Fecha_Nacimiento")
+                    .HasColumnType("time");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasColumnType("tinytext")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                entity.Property(e => e.Provincia)
+                    .IsRequired()
+                    .HasColumnType("text")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                entity.Property(e => e.Telefono)
+                    .IsRequired()
+                    .HasColumnType("tinytext")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                entity.Property(e => e._1eraDosis)
+                    .HasColumnName("1era_Dosis")
+                    .HasColumnType("time");
+
+                entity.Property(e => e._2daDosis)
+                    .HasColumnName("2da_Dosis")
+                    .HasColumnType("time");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
