2dc8dd36a77a59f9548772530b8889f1842aaf32
Eimy C. Olivo G <20197830@gmail.com>
2021-03-19 21:33:26 -0400

    Maps comenzado

--- RegistroVacunas/Models/Client.cs
+++ RegistroVacunas/Models/Client.cs
@@ -22,6 +22,10 @@ namespace RegistroVacunas.Models
         public string FechaNacimiento { get; set; }
         public string Provincia { get; set; }
 
+        public string Longitud { get; set; }
+
+        public string latitud { get; set; }
+
         public virtual ICollection<Vacunas> Vacunas { get; set; }
     }
 }
